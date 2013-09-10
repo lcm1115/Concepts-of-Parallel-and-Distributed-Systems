bcb85fa57ddd550a948bab2b93491718fe2c3558
Liam Morris <lcm1115@rit.edu>
2013-09-10 13:50:09 -0400

    Added Javadoc comments.

--- project1/MiniBlast.java
+++ project1/MiniBlast.java
@@ -1,9 +1,20 @@
 /**
+ * Contains main method for running MiniBlast test on DNA strands.
+ *
  * @author Liam Morris
  */
 class MiniBlast {
+    /**
+     * Prevents instantiation of class.
+     */
     private MiniBlast() {}
 
+    /**
+     * Main method for running MiniBlast on DNA strands.
+     *
+     * @param args command line args for program. First argument should be path
+     *             to input file, second argment should be query to search for.
+     */
     public static void main(String[] args) {
         if (args.length < 2) {
             System.err.println("usage: java MiniBlast <dbfile> <query>");
