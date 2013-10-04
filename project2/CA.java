c51c96106a097d64308db0fc324fcfe00a7f1857
Liam Morris <lcm1115@rit.edu>
2013-10-04 09:38:50 -0400

    Added javadoc comments, fixed lines > 80 chars

--- project2/CA.java
+++ project2/CA.java
@@ -1,29 +1,59 @@
 import edu.rit.pj2.Loop;
 import edu.rit.pj2.Task;
 
+/**
+ * This class contains the main function for a representation of a cellular
+ * automaton. Given an initial array of cells, an update rule, and a number of
+ * steps to run, the program will print out the smallest, largest, and final
+ * populations and the steps at which they occurred.
+ *
+ * @author Liam Morris (lcm1115)
+ */
 public class CA extends Task {
+    // Instance of class that contains the actual data.
     private CellArray ca;
+
+    /**
+     * Main method for running cellular automaton.
+     *
+     * @param args command line args for program.
+     *             First arg should be an string of 0's and 1's (length 8) which
+     *               represents the update rule.
+     *             Second arg should be the size of the cell array.
+     *             Third arg should be the number of steps to run.
+     *             All following args should be indices that start with a live
+     *               cell.
+     */
     public void main(String[] args) {
+        // Check that usage is correct.
         if (args.length < 4) {
-            System.err.println("usage: java CA <rule> <N> <S> <index> ...");
+            System.err.println(
+                    "usage: java CA <rule> <N> <S> <index> ...");
         } else if (args[0].length() != 8) {
-            System.err.println("error - rule must be eight characters long");
-        } else if (args[0].replaceAll("0", "").replaceAll("1", "").length() > 0) {
-            System.err.println("error - rule must contain only 0 and 1 characters");
+            System.err.println(
+                    "error - rule must be eight characters long");
+        } else if (
+                args[0].replaceAll("0", "").replaceAll("1", "").length() > 0) {
+            System.err.println(
+                    "error - rule must contain only 0 and 1 characters");
         } else if (Integer.parseInt(args[1]) <= 0) {
             System.err.println("error - N must be greater than zero");
         } else if (Integer.parseInt(args[2]) <= 0) {
             System.err.println("error - S must be greater than zero");
         } else {
-            int largestPop, smallestPop, largestPopGen, smallestPopGen, finalPop;
-            largestPop = smallestPop = largestPopGen = smallestPopGen = finalPop = 0;
+            // Initialize all values.
+            int largestPop, smallestPop, finalPop;
+            int largestPopGen, smallestPopGen;
+            largestPop = smallestPop = finalPop = 0;
+            largestPopGen = smallestPopGen = 0;
+
             // Get update rule.
             int[] update = new int[8];
             for (int i = 0; i < args[0].length(); ++i) {
                 update[i] = Character.getNumericValue(args[0].charAt(i));
             }
 
-            // Construct initial cell arrays.
+            // Construct initial cell array.
             int[] currentCells = new int[Integer.parseInt(args[1])];
             for (int i = 3; i < args.length; ++i) {
                 currentCells[Integer.parseInt(args[i])] = 1;
@@ -32,6 +62,7 @@ public class CA extends Task {
             ca = new CellArray(update, currentCells);
             largestPop = smallestPop = ca.getPopulation();
             
+            // Run steps on the CellArray.
             int steps = Integer.parseInt(args[2]);
             for (int i = 0; i < steps; ++i) {
                 parallelFor(0, currentCells.length - 1).exec(new Loop() {
@@ -42,6 +73,9 @@ public class CA extends Task {
                         }
                     }
                 });
+
+                // After each step, store the result in the CellArray and check
+                // if either largestPop or smallestPop should be changed.
                 ca.swapArrays();
                 int pop = ca.getPopulation();
                 if (pop > largestPop) {
@@ -55,9 +89,12 @@ public class CA extends Task {
 
             finalPop = ca.getPopulation();
 
-            System.out.println("Smallest popcount: " + smallestPop + " at step " + smallestPopGen);
-            System.out.println("Largest popcount: " + largestPop + " at step " + largestPopGen);
-            System.out.println("Final popcount: " + finalPop + " at step " + steps);
+            System.out.println("Smallest popcount: " + smallestPop +
+                               " at step " + smallestPopGen);
+            System.out.println("Largest popcount: " + largestPop +
+                               " at step " + largestPopGen);
+            System.out.println("Final popcount: " + finalPop + 
+                               " at step " + steps);
         }
     }
 }
--- project2/CellArray.java
+++ project2/CellArray.java
@@ -1,16 +1,44 @@
+/**
+ * Class that is used for storing array for the cellular automaton.
+ * Contains supporting functions for getting the next step of the array and then
+ * updating the array.
+ *
+ * @author Liam Morris (lcm1115)
+ */
 public class CellArray {
+    /**
+     * Array in which the next step is stored.
+     */
     private int[] nextCells;
-    private int[] currentCells;
-    private int[] update;
-    private int generation;
 
+    /**
+     * Array in which the current state is tsored.
+     */
+    private int[] currentCells;
+
+    /**
+     * Update rule for the automaton.
+     */
+    private int[] update;
+
+    /**
+     * Constructor for CellArray.
+     *
+     * @param update update rule for automaton
+     * @param currentCells initial cell array for automaton
+     */
     public CellArray(int[] update, int[] currentCells) {
         this.update = update;
         this.currentCells = currentCells;
         nextCells = new int[currentCells.length];
     }
 
-    int getPopulation() {
+    /**
+     * Counts the number of live cells in the automaton.
+     *
+     * @return number of live cells in the automaton
+     */
+    public int getPopulation() {
         int count = 0;
         for (int i : currentCells) {
             count += i;
@@ -18,21 +46,43 @@ public class CellArray {
         return count;
     }
 
-    void swapArrays() {
+    /**
+     * Swaps the arrays that store the next step and current state.
+     */
+    public void swapArrays() {
         int[] temp = currentCells;
         currentCells = nextCells;
         nextCells = temp;
     }
-    
-    int getNextValue(int index) {
-        int left = currentCells[(index - 1 + currentCells.length) % currentCells.length];
+   
+    /**
+     * Gets the value of a given index after one step is applied to the
+     * automaton.
+     *
+     * @param index the index for which the next value is being computed
+     *
+     * @return the next value (0 or 1) that the given index will have
+     */
+    public int getNextValue(int index) {
+        // Get left, center, and right values.
+        int length = currentCells.length;
+        int left = currentCells[(index - 1 + length) % length];
         int center = currentCells[index];
-        int right = currentCells[(index + 1) % currentCells.length];
+        int right = currentCells[(index + 1) % length];
+
+        // Get next value (based on 3-bit value from left, center, right).
         int updateIndex = (left << 2) + (center << 1) + right;
         return update[updateIndex];
     }
 
-    void setIndex(int index, int value) {
+    /**
+     * Sets the value of an index in the array for the next step to a given
+     * value.
+     *
+     * @param index the index being updated
+     * @param value the value to store in the index
+     */
+    public void setIndex(int index, int value) {
         nextCells[index] = value;
     }
 }
